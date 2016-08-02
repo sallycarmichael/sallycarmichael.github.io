a0be55534fd5061e9a8e8a2150866f829c5a6b96
sallycarmichael <carmichaelsally@gmail.com>
2016-08-01 21:30:51 -0700

    Hamburger and Modal Added
    Clicking hamburger now works as functioning modal. Hamburger color
    changes on click.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
-//NAV//
+//MODAL//
+// when the user clicks on the .trigger button
+$('.trigger').on('click', function () {
+	// add the .show-modal class to the .modal
+	$('.modal').toggleClass('show-modal');
+});
+
+
+
+//HAMBURGER NAV/
 
 // When the user clicks on the hamburger-icon
 $('.hamburger-icon').on('click', function (e) {
